847513621bcb0b9dda79c3f3505a85204c0e69fe
Matthew Yu <MatthewYu@Matthews-MacBook-Air-70.local>
2020-02-01 01:19:53 +0800

    add count function and connect to Redis

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -4,25 +4,33 @@ import redis
 
 
 # fill in the following.
-# HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
 HOST = "redis-15505.c51.ap-southeast-2-1.ec2.cloud.redislabs.com"
-# PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
 PWD = "6sWrOl7eJKwFiWW6SMO844tgkTZIhcYa"
-PORT = "15505" 
+PORT = "15505"
 
-redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
+# HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
+# PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
+# PORT = "11363" 
+
+redis1 = redis.Redis(host=HOST, password=PWD, port=PORT)
 
 while True:
-    msg = input("Please enter your query (type 'quit' or 'exit' to end):").strip()
+    msg = input(
+        "Please enter your query (type 'quit' or 'exit' to end):").strip()
     if msg == 'quit' or msg == 'exit':
         break
     if msg == '':
         continue
-    print("You have entered " + msg, end=' ') 
+    print("You have entered " + msg, end=' ')
 
-   
-    # Add your code here
+    n = 0
+    if redis1.get(msg) is not None:
+        n = int(redis1.get(msg))
 
 
+    value = 1 + n
+    redis1.set(msg, value)
+ 
+    print('for '+ str(value) +' times')
 
-    print('for X times')
+    # print('for X times')
