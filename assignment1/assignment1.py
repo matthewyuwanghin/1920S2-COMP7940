bf8be7a9effad5136233e31fd1c05e4649ab5368
Matthew Yu <MatthewYu@Matthews-MacBook-Air-70.local>
2020-01-31 23:02:53 +0800

    Halo

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -4,9 +4,11 @@ import redis
 
 
 # fill in the following.
-HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
-PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
-PORT = "11363" 
+# HOST = "redis-11363.c1.asia-northeast1-1.gce.cloud.redislabs.com"
+HOST = "redis-15505.c51.ap-southeast-2-1.ec2.cloud.redislabs.com"
+# PWD = "1nOA0St0I7p9pQqu8HkQ18XqDfnoPeoL"
+PWD = "6sWrOl7eJKwFiWW6SMO844tgkTZIhcYa"
+PORT = "15505" 
 
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
 
